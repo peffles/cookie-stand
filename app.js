975c6e791c77ff5b2dd7156f5d404957a4bd7f0e
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2017-11-14 23:39:31 -0800

    added index.html, made other minor changes to app.js

--- app.js
+++ app.js
@@ -1,91 +1,91 @@
-'use strict'
+'use strict';
 
 //Global Variable
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var firstAndPike = {
-    name: 'First & Pike',
-    minCust: 23,
-    maxCust: 65,
-    avgSale: 6.3,
-    salesPerHour: [],
-    randCust: function() {
-        var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-        return rand;
-    },
-    genSalesPerHour: function() {
-        for (var i = 0; i < storeHours.length; i++) {
-            this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-        }
+  name: 'First & Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3,
+  salesPerHour: [],
+  randCust: function() {
+    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    return rand;
+  },
+  genSalesPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
     }
+  }
 };
 
 var seaTac = {
-    name: 'Sea Tac',
-    minCust: 3,
-    maxCust: 24,
-    avgSale: 1.2,
-    salesPerHour: [],
-    randCust: function() {
-        var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-        return rand;
-    },
-    genSalesPerHour: function() {
-        for (var i = 0; i < storeHours.length; i++) {
-            this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-        }
+  name: 'Sea Tac',
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+  salesPerHour: [],
+  randCust: function() {
+    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    return rand;
+  },
+  genSalesPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
     }
+  }
 };
 
 var seattleCenter = {
-    name: 'Seatte Center',
-    minCust: 11,
-    maxCust: 38,
-    avgSale: 3.7,
-    salesPerHour: [],
-    randCust: function() {
-        var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-        return rand;
-    },
-    genSalesPerHour: function() {
-        for (var i = 0; i < storeHours.length; i++) {
-            this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-        }
+  name: 'Seatte Center',
+  minCust: 11,
+  maxCust: 38,
+  avgSale: 3.7,
+  salesPerHour: [],
+  randCust: function() {
+    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    return rand;
+  },
+  genSalesPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
     }
+  }
 };
 
 var capitolHill = {
-    name: 'Capitol Hill',
-    minCust: 20,
-    maxCust: 38,
-    avgSale: 2.3,
-    salesPerHour: [],
-    randCust: function() {
-        var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-        return rand;
-    },
-    genSalesPerHour: function() {
-        for (var i = 0; i < storeHours.length; i++) {
-            this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-        }
+  name: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgSale: 2.3,
+  salesPerHour: [],
+  randCust: function() {
+    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    return rand;
+  },
+  genSalesPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
     }
+  }
 };
 
 var alki = {
-    name: 'Alki',
-    minCust: 2,
-    maxCust: 16,
-    avgSale: 4.6,
-    salesPerHour: [],
-    randCust: function() {
-        var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-        return rand;
-    },
-    genSalesPerHour: function() {
-        for (var i = 0; i < storeHours.length; i++) {
-            this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-        }
+  name: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgSale: 4.6,
+  salesPerHour: [],
+  randCust: function() {
+    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    return rand;
+  },
+  genSalesPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
     }
+  }
 };
 
 function renderHours(hours) {
@@ -95,7 +95,7 @@ function renderHours(hours) {
   head.appendChild(td);
 
   for (var i = 0; i < hours.length; i++) {
-    var td = document.createElement('td');
+    td = document.createElement('td');
     td.append(hours[i]);
     head.appendChild(td);
   }
@@ -104,22 +104,21 @@ function renderHours(hours) {
 }
 
 function render(storeObject) {
-    storeObject.genSalesPerHour();
-    var tBod = document.createElement('tbody');
-    var hoursRow = document.createElement('tr');
-    var tableRow = document.createElement('tr');
-    var rowTitle = document.createElement('td');
-    rowTitle.append(storeObject.name);
-    tableRow.appendChild(rowTitle)
-    for (var i = 0; i < storeObject.salesPerHour.length; i++) {
-        var td = document.createElement('td');
-        td.append(storeObject.salesPerHour[i]);
-        tableRow.appendChild(td)
-    }
+  storeObject.genSalesPerHour();
+  var tBod = document.createElement('tbody');
+  var tableRow = document.createElement('tr');
+  var rowTitle = document.createElement('td');
+  rowTitle.append(storeObject.name);
+  tableRow.appendChild(rowTitle);
+  for (var i = 0; i < storeObject.salesPerHour.length; i++) {
+    var td = document.createElement('td');
+    td.append(storeObject.salesPerHour[i]);
+    tableRow.appendChild(td);
+  }
 
-    tBod.appendChild(tableRow);
+  tBod.appendChild(tableRow);
 
-    document.getElementById('table1').appendChild(tBod);
+  document.getElementById('table1').appendChild(tBod);
 }
 
 renderHours(storeHours);
