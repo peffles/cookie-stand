657929572efee3500a0645b55644e0e37430c65e
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2017-11-15 19:15:47 -0800

    added constructor functions

--- app.js
+++ app.js
@@ -2,128 +2,54 @@
 
 //Global Variable
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var shops = [];
+function Store(name, maxCust, minCust, avgSale) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgSale = avgSale;
+  this.salesPerHour = [];
+  this.totalCookies = 0;
+  shops.push(this);
+  this.calcSalesPerHour();
+  this.totalCookies;
+  this.render();
 
-var firstAndPike = {
-  name: 'First & Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgSale: 6.3,
-  salesPerHour: [],
-  randCust: function() {
-    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-    return rand;
-  },
-  genSalesPerHour: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-    }
-  }
-};
-
-var seaTac = {
-  name: 'Sea Tac',
-  minCust: 3,
-  maxCust: 24,
-  avgSale: 1.2,
-  salesPerHour: [],
-  randCust: function() {
-    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-    return rand;
-  },
-  genSalesPerHour: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-    }
-  }
-};
-
-var seattleCenter = {
-  name: 'Seatte Center',
-  minCust: 11,
-  maxCust: 38,
-  avgSale: 3.7,
-  salesPerHour: [],
-  randCust: function() {
-    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-    return rand;
-  },
-  genSalesPerHour: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-    }
-  }
-};
-
-var capitolHill = {
-  name: 'Capitol Hill',
-  minCust: 20,
-  maxCust: 38,
-  avgSale: 2.3,
-  salesPerHour: [],
-  randCust: function() {
-    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-    return rand;
-  },
-  genSalesPerHour: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-    }
-  }
-};
-
-var alki = {
-  name: 'Alki',
-  minCust: 2,
-  maxCust: 16,
-  avgSale: 4.6,
-  salesPerHour: [],
-  randCust: function() {
-    var rand = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust));
-    return rand;
-  },
-  genSalesPerHour: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      this.salesPerHour.push(Math.floor(this.randCust() * this.avgSale));
-    }
-  }
-};
-
-function renderHours(hours) {
-  var head = document.createElement('thead');
-  var td = document.createElement('td');
-  td.append('Store Hours');
-  head.appendChild(td);
-
-  for (var i = 0; i < hours.length; i++) {
-    td = document.createElement('td');
-    td.append(hours[i]);
-    head.appendChild(td);
-  }
-
-  document.getElementById('table1').appendChild(head);
 }
+Store.prototype.calcSalesPerHour = function() {
+  for (var i = 0; i < storeHours.length; i++) {
+    this.salesPerHour.push(Math.floor(this.calcCustPerHour() * this.avgSale));
+    this.totalCookies += this.salesPerHour[i];
+  }
 
-function render(storeObject) {
-  storeObject.genSalesPerHour();
+  return this.calcSalesPerHour, this.totalCookies;
+
+};
+Store.prototype.calcCustPerHour = function() {
+  return rng(this.minCust, this.maxCust);
+};
+Store.prototype.render = function() {
   var tBod = document.createElement('tbody');
   var tableRow = document.createElement('tr');
   var rowTitle = document.createElement('td');
-  rowTitle.append(storeObject.name);
+  rowTitle.append(this.name);
   tableRow.appendChild(rowTitle);
-  for (var i = 0; i < storeObject.salesPerHour.length; i++) {
+  for (var i = 0; i < this.salesPerHour.length; i++) {
     var td = document.createElement('td');
-    td.append(storeObject.salesPerHour[i]);
+    td.append(this.salesPerHour[i]);
     tableRow.appendChild(td);
   }
 
   tBod.appendChild(tableRow);
-
   document.getElementById('table1').appendChild(tBod);
+};
+function rng(minCust, maxCust) {
+  return Math.round(Math.random() * (maxCust - minCust) + minCust);
 }
-
-renderHours(storeHours);
-render(firstAndPike);
-render(seaTac);
-render(seattleCenter);
-render(capitolHill);
-render(alki);
+new Store('1st and Pike', 23, 65, 6.3, 14, [], 0);
+new Store('SeaTac', 3, 24, 1.2, 14, [], 0);
+new Store('Seattle Center', 11, 38, 3.7, 14, [], 0);
+new Store('Capitol Hill', 20, 38, 2.3, 14, [], 0);
+new Store('Alki', 2, 16, 4.6, 14, [], 0);
+console.log('hit',shops);
+console.log('hit',storeHours);
